bac2934641f0807ff6c16bad9beb42b7a7f23a6a
Mehul Singh <mehulsingh072001@gmail.com>
2022-06-04 05:17:20 +0530

    added graph dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react"
+ 
 import AppSidebar from "../Components/Sidebars/AppSidebar"
 import AppTopbar from "../Components/Topbars/AppTopbar"
 
 function Dashboard(){
+  const [pastWeek, setPastWeek] = useState([])
+
+  useEffect(() => {
+    setPastDates()
+  }, [])
+
+  const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+  const setPastDates = () =>{
+    const dates = [...Array(7)].map((_, i) => {
+        const d = new Date()
+        d.setDate(d.getDate() - i)
+        return d
+    })
+    setPastWeek(dates.reverse());
+  }
   return(
     <div className="dashboard">
       <AppSidebar/>
@@ -11,11 +29,24 @@ function Dashboard(){
         <div className="dashboard__container--left">
           <div className="dashboard__container--left--data">
             <div className="topbar">
-              <h2 className="heading-2">Email Sent</h2>
+              <h2 className="heading-2"><i className="fab fa-telegram-plane"></i> Email Sent</h2>
               <div className="sent-number heading-1">0</div>
             </div>
 
-            <div className="data">
+            <div className="data-chart">
+              <div className="data-chart__bars">
+                <div className="data-chart__bars--item" style={{height: "2%"}}>
+                  <div className="data-chart__bars--item--bar" style={{minWidth: "fit-content"}}>
+                  </div>
+                </div>
+              </div>
+              <div className="data-chart__days">
+                {
+                  pastWeek.map((d, i) =>
+                    <div key={i} className="data-chart__days--day">{d.getDate()} {month[d.getMonth()]}</div>
+                  )
+                }
+              </div>
             </div>
           </div>
 
