b285d753bad9759f4dea66307b12d21b83dbfd2b
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2022-06-14 18:26:00 +0530

    added intent detection

--- src/Pages/Settings/ProspectSettings.js
+++ src/Pages/Settings/ProspectSettings.js
@@ -1,10 +1,48 @@
 import {useNavigate} from "react-router-dom"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+
 import AppSidebar from "../../Components/Sidebars/AppSidebar";
 import AppTopbar from "../../Components/Topbars/AppTopbar"
 import ToggleSwitch from "../../Components/ToggleSwitch";
 
+const cookies = new Cookies()
 function ProspectSettings(){ 
   let navigate = useNavigate()
+  const [intent, setIntent] = useState({})
+  const [intentEdited, setIntentEdited] = useState(false)
+
+  useEffect(() => {
+    getIntent()
+  },[])
+
+  const getIntent = () => {
+    axios.get("/api/intent", {
+      headers: {
+        "Authorization": `Bearer ${cookies.get("access_token")}`
+      },
+      params: {
+        "userId": cookies.get("userId")
+      }
+    }).then((res) => {
+      setIntent(res.data)
+    })
+  }
+
+  const updateIntent = () => {
+    axios.put("/api/intent/update", intent, {
+      headers: {
+        "Authorization": `Bearer ${cookies.get("access_token")}`
+      },
+      params: {
+        "intentDetectionId": intent.id
+      }
+    }).then((res) => {
+      getIntent()
+      setIntentEdited(false)
+    })
+  }
 
   return(
     <div className="prospect-settings">
@@ -25,24 +63,24 @@ function ProspectSettings(){
                 <p className="copy__para--medium-light u-margin-top-small">Set points to decide when prospect becomes hot</p>
               </div>
               <div className="row">
-                <button className="btn-sec">Edit</button>
+                <button onClick={() => updateIntent()} className={intentEdited ? "btn" : "btn--disabled"}>Save</button>
                 <ToggleSwitch/>
               </div>
             </div>
 
             <div className="points">
               <p className="copy__para--medium">If a Prospect opens an email, assign</p>
-              <input type="number" value="2"/>
+              <input type="number" defaultValue={intent.openPoints} onChange={(e) => {setIntent({...intent, openPoints: e.target.value}); setIntentEdited(true)}}/>
             </div>
 
             <div className="points">
               <p className="copy__para--medium">If a Prospect clicks a link in an email, assign</p>
-              <input type="number" value="5"/>
+              <input type="number" defaultValue={intent.clickPoints} onChange={(e) => {setIntent({...intent, clickPoints: e.target.value}); setIntentEdited(true)}}/>
             </div>
 
             <div className="points">
               <p className="copy__para--medium">A Prospect becomes Hot if they have atleast</p>
-              <input type="number" value="6"/>
+              <input type="number" defaultValue={intent.hotPoints} onChange={(e) => {setIntent({...intent, hotPoints: e.target.value}); setIntentEdited(true)}}/>
             </div>
           </div>
         </div>
