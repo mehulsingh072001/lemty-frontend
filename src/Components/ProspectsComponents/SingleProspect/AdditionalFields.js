ee2c5fa8d88069d4f61a5c99adf736ed5b2b065e
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2022-06-15 20:49:48 +0530

    prospect update complete

--- src/Components/ProspectsComponents/SingleProspect/AdditionalFields.js
+++ src/Components/ProspectsComponents/SingleProspect/AdditionalFields.js
@@ -1,53 +1,49 @@
-const AdditionalFields = ({prospect}) => {
+const AdditionalFields = ({prospect, setProspect}) => {
   return(
     <>
       <div className="item">
         <label htmlFor="">Account</label>
-        <input type="text" value={prospect.prospectAccount}/>
+        <input type="text" value={prospect.prospectAccount} onChange={(e) => setProspect({...prospect, prospectAccount: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Title</label>
-        <input type="text" value={prospect.prospectTitle}/>
+        <input type="text" value={prospect.prospectTitle} onChange={(e) => setProspect({...prospect, prospectTitle: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Department</label>
-        <input type="text" value={prospect.prospectDepartment} />
+        <input type="text" value={prospect.prospectDepartment} onChange={(e) => setProspect({...prospect, prospectDepartment: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Company Email</label>
-        <input type="text" value={prospect.prospectCompanyEmail}/>
+        <input type="text" value={prospect.prospectCompanyEmail} onChange={(e) => setProspect({...prospect, prospectCompanyEmail: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Company Phone-no</label>
-        <input type="text" value={prospect.prospectCompanyPhone}/>
+        <input type="text" value={prospect.prospectCompanyPhone} onChange={(e) => setProspect({...prospect, prospectCompanyPhone: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Company Domain</label>
-        <input type="text" value={prospect.prospectCompanyDomain}/>
+        <input type="text" value={prospect.prospectCompanyDomain} onChange={(e) => setProspect({...prospect, prospectCompanyDomain: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">LinkedIn URL</label>
-        <input type="text" value={prospect.prospectLinkedinUrl}/>
+        <input type="text" value={prospect.prospectLinkedinUrl} onChange={(e) => setProspect({...prospect, prospectLinkedinUrl: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Twitter-ID</label>
-        <input type="text" value={prospect.prospectTwitterUrl}/>
-      </div>
-      <div className="item">
-        <label htmlFor="">Twitter-ID</label>
-        <input type="text" value={prospect.prospectTwitterUrl}/>
+        <input type="text" value={prospect.prospectTwitterUrl} onChange={(e) => setProspect({...prospect, prospectTwitterUrl: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">City</label>
-        <input type="text" value={prospect.prospectCity}/>
+        <input type="text" value={prospect.prospectCity} onChange={(e) => setProspect({...prospect, prospectCity: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Location</label>
-        <input type="text" value={prospect.prospectLocation}/>
+        <input type="text" value={prospect.prospectLocation} onChange={(e) => setProspect({...prospect, prospectLocation: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Country</label>
-        <input type="text" value={prospect.prospectCountry}/>
+        <input type="text" value={prospect.prospectCountry} onChange={(e) => setProspect({...prospect, prospectCountry: e.target.value})}/>
       </div>
     </>
   )
--- src/Components/ProspectsComponents/SingleProspect/BasicFields.js
+++ src/Components/ProspectsComponents/SingleProspect/BasicFields.js
@@ -7,19 +7,19 @@ const BasicFields = ({prospect, setProspect}) => {
       </div>
       <div className="item">
         <label htmlFor="">Last Name</label>
-        <input type="text" value={prospect.lastName}/>
+        <input type="text" value={prospect.lastName} onChange={(e) => setProspect({...prospect, lastName: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Email</label>
-        <input type="text" value={prospect.prospectEmail}/>
+        <input type="text" value={prospect.prospectEmail} onChange={(e) => setProspect({...prospect, prospectEmail: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Phone no.</label>
-        <input type="text"  value={prospect.prospectMobileNumber}/>
+        <input type="text"  value={prospect.prospectMobileNumber} onChange={(e) => setProspect({...prospect, prospectMobileNumber: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Company</label>
-        <input type="text" value={prospect.prospectCompany}/>
+        <input type="text" value={prospect.prospectCompany} onChange={(e) => setProspect({...prospect, prospectCompany: e.target.value})}/>
       </div>
       <div className="item">
         <label htmlFor="">Tags</label>
@@ -27,7 +27,7 @@ const BasicFields = ({prospect, setProspect}) => {
       </div>
       <div className="item">
         <label htmlFor="">List</label>
-        <input type="text" value={prospect.prospectList}/>
+        <input type="text" value={prospect.prospectList} onChange={(e) => setProspect({...prospect, prospectList: e.target.value})}/>
       </div>
     </>
   )
